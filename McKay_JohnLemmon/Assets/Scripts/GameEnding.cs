5c7f962adc6dff6d5067a797f4050594b0dae200
mckaymac <54556260+mckaymac@users.noreply.github.com>
2020-02-25 13:11:46 -0500

    Added ending screen

---
+++ McKay_JohnLemmon/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+
+    public float fadeDuration = 1f;
+    public float displayDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+
+    //Checks to see if the player reaches the end
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject == player){
+            m_IsPlayerAtExit = true;
+        }
+    }
+
+    void Update(){
+        if(m_IsPlayerAtExit){
+            EndLevel();
+        }
+    }
+
+    //Puts up the end screen for a time when the player reaches the end and quits the program
+    void EndLevel(){
+        m_Timer += Time.deltaTime;
+
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if(m_Timer > fadeDuration + displayDuration){
+            Application.Quit();
+        }
+    }
+}
