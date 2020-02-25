22ca92e1e9adacb75d2b2d099a2da08a47b47f8f
mckaymac <54556260+mckaymac@users.noreply.github.com>
2020-02-25 14:18:38 -0500

    Added audio to ghosts and john lemmon

--- McKay_JohnLemmon/Assets/Scripts/PlayerMovement.cs
+++ McKay_JohnLemmon/Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,10 @@ public class PlayerMovement : MonoBehaviour
     public float turnSpeed = 20f;
     Animator m_Animator;
     Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
     Vector3 m_Movement;
     Quaternion m_Rotation = Quaternion.identity;
+
     
 
     // Start is called before the first frame update
@@ -16,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
     {
         m_Animator = GetComponent<Animator>();
         m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -35,6 +38,15 @@ public class PlayerMovement : MonoBehaviour
         //Starts the walking animation if logic is right
         bool isWalking = hasHorizontalInput || hasVerticalInput;
         m_Animator.SetBool("isWalking", isWalking);
+
+        if(isWalking){
+            if(!m_AudioSource.isPlaying){
+                m_AudioSource.Play();
+            }
+        }
+        else{
+            m_AudioSource.Stop();
+        }
 
         Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
         m_Rotation = Quaternion.LookRotation(desiredForward);
